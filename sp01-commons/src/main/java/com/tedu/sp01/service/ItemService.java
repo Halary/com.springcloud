0a67c4f8e58c1a9bdb574709a3e395b7efd511c9
Halary <52346470+Halary@users.noreply.github.com>
2019-08-02 09:59:30 +0800

    Add files via upload

---
+++ sp01-commons/src/main/java/com/tedu/sp01/service/ItemService.java
@@ -0,0 +1,14 @@
+package com.tedu.sp01.service;
+
+
+
+import java.util.List;
+
+import com.tedu.sp01.pojo.Item;
+
+public interface ItemService {
+	List<Item> getIteams(String orderId);
+	void decreaseNumbers(List<Item> list);
+	List<Item> getItems(String orderId);
+
+}
